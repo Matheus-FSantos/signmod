c7d7241274e983e41af77133efb0749a53fd25f9
Matheus-FSantos <matheus.fs.contato@gmail.com>
2026-06-06 15:52:03 -0300

    chore: init repo with base project

---
+++ src/main/java/io/github/matheus_fsantos/signmod/SignmodApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.signmod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignmodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignmodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/matheus_fsantos/signmod/SignmodApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.signmod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignmodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
